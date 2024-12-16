fb5235d7a993447a9372f9eb58194d9f0f98babb
Lucas Boucher <contact@lucasboucher.fr>
2024-12-16 13:48:30 +0100

    feat: two main tabs on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,30 @@
-import { Button } from '@/components/ui/button';
-
-import { LayoutDashboard } from 'lucide-react';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { CctvIcon, BikeIcon } from 'lucide-react';
 
 export default function Home() {
   return (
     <main className="container mx-auto">
-      <div className="flex justify-center items-center h-screen">
-        <Button variant="outline" size="sm">
-          <LayoutDashboard /> Click me
-        </Button>
+      <div className="flex justify-center items-center pt-8">
+        <Tabs defaultValue="crime">
+          <TabsList className="mb-2">
+            <TabsTrigger value="crime" className="md:w-64">
+              <CctvIcon size={16} strokeWidth={1} className="mr-1" />
+              Criminalité
+            </TabsTrigger>
+            <TabsTrigger value="bicycle" className="md:w-64">
+              <BikeIcon size={16} strokeWidth={1} className="mr-1" />
+              Vélos
+            </TabsTrigger>
+          </TabsList>
+          <div className="bg-muted text-muted-foreground rounded-lg py-4">
+            <TabsContent value="crime" className="flex justify-center m-0">
+              <p className="italic">Onglet pour la criminalité</p>
+            </TabsContent>
+            <TabsContent value="bicycle" className="flex justify-center m-0">
+              <p className="italic">Onglet pour les vélos</p>
+            </TabsContent>
+          </div>
+        </Tabs>
       </div>
     </main>
   );
