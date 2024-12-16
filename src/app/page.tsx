50fb02a233b7b4847e6174d39ceabb65137acf2a
Lucas Boucher <contact@lucasboucher.fr>
2024-12-16 14:53:49 +0100

    feat: update template and add title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,24 +5,25 @@ import { CctvIcon, BikeIcon } from 'lucide-react';
 
 export default function Home() {
   return (
-    <main className="container mx-auto">
-      <div className="flex items-center justify-center pt-8">
+    <main className="container mx-auto max-w-screen-lg px-4 py-8">
+      <h1 className="mb-2 text-3xl font-bold">NYC Urban Data</h1>
+      <div>
         <Tabs defaultValue="crime">
-          <TabsList className="mb-2">
-            <TabsTrigger value="crime" className="md:w-64">
+          <TabsList className="w-full">
+            <TabsTrigger value="crime" className="w-1/2">
               <CctvIcon size={16} strokeWidth={1} className="mr-1" />
               Criminalité
             </TabsTrigger>
-            <TabsTrigger value="bicycle" className="md:w-64">
+            <TabsTrigger value="bicycle" className="w-1/2">
               <BikeIcon size={16} strokeWidth={1} className="mr-1" />
               Vélos
             </TabsTrigger>
           </TabsList>
-          <div className="rounded-lg bg-muted py-4 text-muted-foreground">
-            <TabsContent value="crime" className="m-0 flex justify-center">
+          <div className="rounded-lg bg-muted text-muted-foreground">
+            <TabsContent value="crime">
               <CrimeTab />
             </TabsContent>
-            <TabsContent value="bicycle" className="m-0 flex justify-center">
+            <TabsContent value="bicycle">
               <BicycleTab />
             </TabsContent>
           </div>
