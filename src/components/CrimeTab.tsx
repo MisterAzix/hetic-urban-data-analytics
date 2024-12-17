a4e0610d1fc01101c4e6472c37af4473fd8be3e5
Lucas Boucher <contact@lucasboucher.fr>
2024-12-17 14:02:51 +0100

    feat: add leaflet map (#13)
    * feat: add leaflet map
    
    * feat: block style adjustments
    
    * feat: ney work coordinates and new map provider
    
    * feat: heatmap and delete leaflet react for it
    
    * fix: unused warn and prettier shadcn
    
    * fix: leaflet types on dev dependencies

--- src/components/CrimeTab.tsx
+++ src/components/CrimeTab.tsx
@@ -6,11 +6,11 @@ import CrimeTypeChart from './charts/CrimeTypeChart';
 export default function CrimeTab() {
   return (
     <>
-      <div className="border-b-8 border-white px-4 py-8">
+      <div className="mb-2 rounded-lg bg-muted px-4 py-8">
         <h2 className="mb-4 text-2xl font-semibold">Type de criminalité</h2>
         <CrimeTypeChart />
       </div>
-      <div className="px-4 py-8">
+      <div className="rounded-lg bg-muted px-4 py-8">
         <h2 className="mb-4 text-2xl font-semibold">Graphique basique</h2>
         <BasicChart />
       </div>
