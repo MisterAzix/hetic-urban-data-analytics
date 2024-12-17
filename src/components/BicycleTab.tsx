fc36797791150989f3cf012e5b43df97b48b9f24
Lucas Boucher <contact@lucasboucher.fr>
2024-12-17 11:13:15 +0100

    feat: add leaflet map

--- src/components/BicycleTab.tsx
+++ src/components/BicycleTab.tsx
@@ -1,7 +1,21 @@
+'use client';
+
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css';
+import 'leaflet-defaulticon-compatibility';
+
+import { LatLngExpression } from 'leaflet';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+
 export default function BicycleTab() {
+  const mainPosition: LatLngExpression = [48.864716, 2.349014];
+
   return (
-    <div className="py-4">
-      <p className="text-center italic">Onglet pour les vélos</p>
-    </div>
+    <MapContainer center={mainPosition} zoom={10} className="h-[512px] w-full">
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <Marker position={mainPosition}>
+        <Popup>Paris</Popup>
+      </Marker>
+    </MapContainer>
   );
 }
