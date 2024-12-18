aac044f41d6c3c4d15b9dddf95e0686de1b00751
Maxence Breuilles <40914400+MisterAzix@users.noreply.github.com>
2024-12-18 11:07:03 +0100

    feat: refactor api root with bike and crime services ♻️ (#18)

--- src/app/api/bikes/route.ts
+++ src/app/api/bikes/route.ts
@@ -1,10 +1,12 @@
-import prisma from '@/lib/prisma';
 import { NextResponse } from 'next/server';
+import { BikeService } from '@/servcices/bike.service';
 
 // GET: Retrieve all bike stations from the database
 export async function GET() {
+  const bikeService = new BikeService(process.env.BIKE_API_URL);
+
   try {
-    const bikeStations = await prisma.bikeStation.findMany();
+    const bikeStations = await bikeService.getBikeStations();
     return NextResponse.json(bikeStations);
   } catch (error) {
     console.error('Error retrieving bike stations:', error);
--- src/app/api/crimes/fetch/route.ts
+++ src/app/api/crimes/fetch/route.ts
@@ -1,43 +1,17 @@
-import { formatAgeGroup, formatBorough } from '@/lib/crimes.';
-import prisma from '@/lib/prisma';
 import { NextResponse } from 'next/server';
+import { CrimeService } from '@/servcices/crime.service';
 
 export async function POST() {
-  const data = await fetch(
-    'https://data.cityofnewyork.us/resource/mv4k-y93f.json',
-  );
-  const crimes = await data.json();
-  console.log('crimes', crimes);
+  const crimeService = new CrimeService(process.env.CRIME_API_URL);
 
   try {
-    for (const crime of crimes) {
-      const ageGroup = formatAgeGroup(crime.age_group);
-      const borough = formatBorough(crime.boro);
-      const summonsDate = new Date(crime.summons_date);
-
-      await prisma.crime.create({
-        data: {
-          external_id: crime.summons_key,
-          latitude: crime.latitude,
-          longitude: crime.longitude,
-          age_group: ageGroup,
-          sex: crime.sex,
-          race: crime.race,
-          summons_date: summonsDate,
-          offense_description: crime.offense_description,
-          borough: borough,
-        },
-      });
-    }
-
-    return NextResponse.json({ message: 'Crimes created' });
+    await crimeService.createCrimes();
   } catch (error) {
     console.error('Error creating crimes:', error);
-    const errorMessage =
-      error instanceof Error ? error.message : 'Unknown error';
     return NextResponse.json(
-      { error: 'Failed to create crimes', details: errorMessage },
+      { error: 'Failed to create crimes' },
       { status: 500 },
     );
   }
+  return NextResponse.json({ message: 'Crimes created!' });
 }
