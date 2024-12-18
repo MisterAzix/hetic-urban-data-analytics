6de7fe1808866d77c42767a7b43f5a4b2c3335fa
Lounis Ourrad <109440184+LostLemonCurd@users.noreply.github.com>
2024-12-19 00:22:23 +0100

    feat(stations-history): added a history table for bike stations (#28)

---
+++ prisma/migrations/20241218154945_altered_history_table_to_lighten_the_data/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `total_capacity` on the `BikeStationHistory` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "BikeStationHistory" DROP COLUMN "total_capacity";
