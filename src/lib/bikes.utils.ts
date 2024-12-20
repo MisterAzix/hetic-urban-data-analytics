ccaf7c8825f65f562b31f5088833eca8a33e464e
Lucas Boucher <contact@lucasboucher.fr>
2024-12-20 12:07:30 +0100

    feat: history and sorting

--- src/lib/bikes.utils.ts
+++ src/lib/bikes.utils.ts
@@ -1,27 +1,42 @@
 import { BikeStationData } from '@/components/tabs/BikeTab';
-import { BikeStation } from '@prisma/client';
+import { BikeStation, BikeStationHistory } from '@prisma/client';
 
 export const fetchBikeStationData = async () => {
   const data = await fetch('http://localhost:3000/api/bikes/', {
     cache: 'no-store',
   });
   const bikes = await data.json();
+
   const bikeStationData: BikeStationData[] = [];
   const bikeStationUniqueDate: string[] = [];
+
   bikes.forEach((bike: BikeStation) => {
-    const date = bike.timestamp.toString().split(':').slice(0, 2).join(':'); // Extract date and time up to minutes
-    if (!bikeStationUniqueDate.includes(date)) {
-      bikeStationUniqueDate.push(date);
-      bikeStationData.push({
-        date: date,
-        free_bikes: bike.free_bikes,
-        empty_slots: bike.empty_slots,
-      });
-    } else {
-      const dateIndex = bikeStationData.findIndex((data) => data.date === date);
-      bikeStationData[dateIndex].free_bikes += bike.free_bikes;
-      bikeStationData[dateIndex].empty_slots += bike.empty_slots;
-    }
+    bike.BikeStationHistory.forEach((bikeHistory: BikeStationHistory) => {
+      const date = bikeHistory.timestamp
+        .toString()
+        .split(':')
+        .slice(0, 2)
+        .join(':');
+      if (!bikeStationUniqueDate.includes(date)) {
+        bikeStationUniqueDate.push(date);
+        bikeStationData.push({
+          date: date,
+          free_bikes: bikeHistory.free_bikes,
+          empty_slots: bikeHistory.empty_slots,
+        });
+      } else {
+        const dateIndex = bikeStationData.findIndex(
+          (data) => data.date === date,
+        );
+        bikeStationData[dateIndex].free_bikes += bikeHistory.free_bikes;
+        bikeStationData[dateIndex].empty_slots += bikeHistory.empty_slots;
+      }
+    });
   });
+
+  bikeStationData.sort(
+    (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime(),
+  );
+
   return bikeStationData;
 };
